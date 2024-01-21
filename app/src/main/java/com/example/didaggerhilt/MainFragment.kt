78b42a958047ee36aa5c073fa4a8a0491fbaa8bb
manibharathi <manibharathir@sugunafoods.com>
2024-01-21 10:34:44 +0530

    Fragment Injection

--- app/src/main/java/com/example/didaggerhilt/MainFragment.kt
+++ app/src/main/java/com/example/didaggerhilt/MainFragment.kt
@@ -2,12 +2,8 @@ package com.example.didaggerhilt
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 
 class MainFragment(
